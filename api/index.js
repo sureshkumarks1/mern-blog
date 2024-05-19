8a3adcc33a0a7e82d30c407b4fa68f772e8ffc72
SURESH KUMAR K S <sureshkumarks1@gmail.com>
2024-05-19 20:35:11 +0530

    create user model

--- api/index.js
+++ api/index.js
@@ -1,7 +1,15 @@
-import express from 'express'
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+mongoose.connect(
+  "mongodb+srv://sureshkumarks1@gmail.com:<password>@cluster0.fy4qlsh.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+);
 
 const app = express();
 
-app.listen(3000,()=>{
-    console.log("Server running in port 3000")
-})
+app.listen(3000, () => {
+  console.log("Server running in port 3000");
+});
